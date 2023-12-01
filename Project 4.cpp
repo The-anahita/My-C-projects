d960c460c0ab6bd4ecb4d2473355655451b0d85a
The-anahita <150476308+The-anahita@users.noreply.github.com>
2023-12-02 01:11:12 +0330

    Add files via upload

---
+++ Project 4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+   char letter;
+   cout << "Please enter an English character in lower case\n";
+   cin >> letter;
+   
+   switch (letter){
+   	    case 'a':
+   		cout<<"VOWEL";
+   		break;
+      case 'e':
+   		cout<<"VOWEL";	
+			break;
+	  case 'i':
+		cout<<"VOWEL";
+		break;
+   	  case 'o':
+		cout<<"VOWEL";
+		break;
+	  case 'u':
+		cout<<"VOWEL";
+			
+   }
+
+}
